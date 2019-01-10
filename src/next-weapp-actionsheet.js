4cc11bac9d84e11aaef4528dad15db31a948b66e
afeiship <afeiship@gmail.com>
2019-01-10 16:51:12 +0800

    revert: to wxproise

--- src/next-weapp-actionsheet.js
+++ src/next-weapp-actionsheet.js
@@ -1,23 +1,14 @@
 (function() {
   var global = global || this || window || Function('return this')();
   var nx = global.nx || require('next-js-core2');
+  var nxWxPromisify = nx.wxPromisify || require('next-wx-promisify');
   var wx = global.wx;
 
   var NxWeappActionsheet = nx.declare('nx.WeappActionsheet', {
     statics: {
       present: function(inOptions) {
         return new Promise(function(resolve, reject) {
-          var options = nx.mix(
-            {
-              success: function(res) {
-                resolve({ code: 0, data: res });
-              },
-              fail: function(res) {
-                resolve({ code: 1, data: res });
-              }
-            },
-            inOptions
-          );
+          var options = nx.mix(nxWxPromisify(resolve, reject), inOptions);
           wx.showActionSheet(options);
         });
       },
